753977707d33bcbeb4df3a24c9c5b72ede5c2bc1
priv <140729444+scriptprivate@users.noreply.github.com>
2024-06-18 10:19:03 -0300

    feat(filter): add support for filtering files by extension

--- prv.pl
+++ prv.pl
@@ -7,18 +7,36 @@ use Term::ReadLine;
 use Try::Tiny;
 
 sub main {
-    my $dir = shift || '.';
     my $keep_ext = 0;
-    GetOptions('k|keep' => \$keep_ext);
+    my @filters;
+
+    GetOptions(
+        'k|keep' => \$keep_ext,
+        'f|filter=s{1,}' => \@filters
+    );
+
+    my $dir = shift @ARGV || '.';
 
     opendir(my $dh, $dir) or die "\nCan't open directory '$dir': $!\n\n";
     my @files = grep { $_ ne '.' && $_ ne '..' } readdir($dh);
     closedir($dh);
 
+    if (@filters) {
+        @files = grep {
+            my $file = $_;
+            grep { $file =~ /\Q$_\E$/ } @filters
+        } @files;
+    }
+
     my $term = Term::ReadLine->new('file_rename');
     my $OUT = $term->OUT || \*STDOUT;
 
     print $OUT "\nCurrent files in directory '$dir':\n\n";
+    unless (@files) {
+        print $OUT "\nNo files found with the specified extensions.\n\n";
+        return 1;
+    }
+    
     print $OUT join("\n", @files) . "\n";
     print $OUT "\nType the new names, one per line. Leave blank to keep the same name.\n\n";
 
